92fd4af4fb1210edeab5fc68f112ca3eb9d730df
ALSABA3 <202003001107@dogus.edu.tr>
2023-08-07 06:28:27 +0300

    more details add on courses

--- Client/src/pages/2.Courses/Courses.jsx
+++ Client/src/pages/2.Courses/Courses.jsx
@@ -3,6 +3,8 @@ import "./Courses.css";
 
 const Courses = () => {
   const [departments, setDepartments] = useState([]);
+  const [selectedDepartment, setSelectedDepartment] = useState(null);
+  const [departmentCourses, setDepartmentCourses] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:5000/api/courses")
@@ -11,28 +13,79 @@ const Courses = () => {
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
+  const fetchCoursesForDepartment = (departmentName) => {
+    fetch(
+      `http://localhost:5000/api/courses/${encodeURIComponent(departmentName)}`
+    )
+      .then((response) => response.json())
+      .then((data) => setDepartmentCourses(data))
+      .catch((error) => console.error("Error fetching courses:", error));
+  };
+
+  const handleDepartmentClick = (department) => {
+    setSelectedDepartment(department);
+    fetchCoursesForDepartment(department.department);
+  };
+
+  const handleBackToDepartments = () => {
+    setSelectedDepartment(null);
+    setDepartmentCourses([]);
+  };
+
   return (
     <>
       <section className="courses">
         <div className="container">
           <div
-            className="d-flex justify-content-center align-items-center"
+            className="d-flex flex-column justify-content-center align-items-center"
             style={{ height: "100vh" }}
           >
-            <div className="department-cards">
-              {departments.map((department, index) => (
-                <a href="#">
-                  <div
+            {selectedDepartment ? (
+              <>
+                <h3 style={{ color: "white" }}>
+                  Courses From {selectedDepartment?.department}
+                </h3>
+                <div className="course-list">
+                  {departmentCourses.map((course, index) => (
+                    <a key={index} className="department-card">
+                      <div className="d-flex align-items-center flex-column">
+                        <h2>{course.course_name}</h2>
+                        <img
+                          src="/black-bg-logo.png"
+                          alt={`Logo for ${course.course_name}`}
+                        />
+                      </div>
+                    </a>
+                  ))}
+                </div>
+                <button
+                  className="btn btn-outline-light py-2"
+                  type="submit"
+                  onClick={handleBackToDepartments}
+                >
+                  Back to Departments
+                </button>
+              </>
+            ) : (
+              <div className="department-cards">
+                {departments.map((department, index) => (
+                  <a
                     key={index}
-                    className="department-card d-flex align-items-center flex-column"
+                    className="department-card"
+                    onClick={() => handleDepartmentClick(department)}
                   >
-                    <h2>{department.department}</h2>
-                    <img src="/black-bg-logo.png" />
-                    {/* You can add more information or styling here */}
-                  </div>
-                </a>
-              ))}
-            </div>
+                    <div className="d-flex align-items-center flex-column">
+                      <h2>{department.department}</h2>
+                      <img
+                        src="/black-bg-logo.png"
+                        alt={`Logo for ${department.department}`}
+                      />
+                      {/* You can add more information or styling here */}
+                    </div>
+                  </a>
+                ))}
+              </div>
+            )}
           </div>
         </div>
       </section>
